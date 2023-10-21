64112b8f47efefc4693d8a7dbabad00bc417b1fc
Jason Jashari <13jasonja@gmail.com>
2023-10-21 22:59:11 +0100

    Update axios BASE_URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'https://projectmanagementapp-b1819bc02f67.herokuapp.com/'
 
 export default axios.create({
     baseURL: BASE_URL
